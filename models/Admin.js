30d0f8a8b492d039634af3ca8439357280137eb5
krish1124007 <148999167+krish1124007@users.noreply.github.com>
2025-02-02 12:56:17 +0530

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -1,12 +1,16 @@
 /** @format */
 import mongoose from 'mongoose';
-
-
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 const adminSchema = new mongoose.Schema(
   {
     companyName: {
       type: String,
       required: [true, 'Company name is required'],
+    },
+    username:{
+      type:String,
+      require:true,
     },
     email: {
       type: String,
@@ -37,6 +41,34 @@ const adminSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+
+adminSchema.pre("save" , async function(next){
+  if(!this.isModified("password")){next()}
+  this.password = await bcrypt.hash(this.password , 10);
+  next();
+})
+
+adminSchema.methods.PassCompare = async function(userPassword)
+{
+  return await bcrypt.compare(userPassword , this.password)
+}
+
+adminSchema.methods.GenrateAccessTocken = function()
+{
+  return jwt.sign(
+    {
+      _id:this._id,
+      username:this.username,
+      email:this.email
+
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn:process.env.JWT_EXPIRE
+    }
+  )
+}
+
 adminSchema.index({ email: 1 }, { unique: true });
 
-export default mongoose.model('Admin', adminSchema);
+export const Admin = mongoose.model('Admin', adminSchema);
