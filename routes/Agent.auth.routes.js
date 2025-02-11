6d42604c21128980a1dbc7f57802b9bd08c4d366
krish1124007 <148999167+krish1124007@users.noreply.github.com>
2025-02-11 07:52:06 +0530

    Update Agent.auth.routes.js
    fix  bug

--- routes/Agent.auth.routes.js
+++ routes/Agent.auth.routes.js
@@ -8,6 +8,7 @@ import {
 } from "../controllers/Agent/agent.auth.controller.js"
 import { GetAgentProfile,
     GetAgentUrl} from "../controllers/Agent/agent.db.controller.js"
+import {UserVerify} from "../middlewares/Uservrfy.js"
 
 import { Router } from "express"
 
@@ -19,7 +20,7 @@ router.route('/checkotp').post(CheckOtp);
 router.route('/register').post(Register);
 router.route('/login').post(Login);
 router.route('/loginVrfy').post(LoginVrfy)
-router.route('/getagentprofile/:id').get(GetAgentUrl)    
+router.route('/getagentprofile/:id').get(UserVerify,GetAgentUrl)    
 
 
 const AgentRoutes = router;
